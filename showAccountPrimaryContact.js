5b0041a5f5ed07b681b8861c843562937367be94
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2025-01-22 17:17:25 +0000

    Fixed error retrieving contacts associated account

--- showAccountPrimaryContact.js
+++ showAccountPrimaryContact.js
@@ -1,8 +1,6 @@
 async function showAccountPrimaryContact(executionContext) {
     var formContext = executionContext.getFormContext();
     
-    executionContext.getFormContext().ui.setFormNotification("v35", "INFO", "SAPC0001");
-    
     try {
         var contact = await retrieveContact(formContext);
         if (contact == null) {
@@ -27,26 +25,31 @@ async function saveContactIfRequired(executionContext) {
     
     var accountId = formContext.getAttribute("customerid").getValue()[0].id;
     var contactId = contactAttribute.getValue()[0].id;
+    
+    //Remove curly brackets at start and end of ids
+    accountId = accountId.slice(1, accountId.length - 1);
+    contactId = contactId.slice(1, contactId.length - 1);
+    
+    //Stop contact being set as the accounts primary contact if
+    //contact is not already associated with account
     if (!await checkAccountOwnsContact(accountId, contactId))
         return;
     
-    await saveAccountPrimaryContact(accountId.slice(1, accountId.length-1), contactId.slice(1, contactId.length-1));
-    
+    await saveAccountPrimaryContact(accountId, contactId);
     toggleContactMandatory(formContext, false);
 }
 
 async function checkAccountOwnsContact(accountId, contactId) {
     try {
-        var contactParentAccountId = (await Xrm.WebApi.retrieveRecord(
+        var contact = (await Xrm.WebApi.retrieveRecord(
             "contact",
-            contactId,
-            "?$select=parentcustomerid"
-        )).parentcustomerid;
+            contactId
+        ));
         
-        if (contactParentAccountId == accountId)
+        if (contact._parentcustomerid_value.toUpperCase() == accountId)
             return true;
-    } catch {
-        Xrm.Navigation.openErrorDialog({ message: "Error saving primary contact to account" });
+    } catch(e) {
+        Xrm.Navigation.openErrorDialog({ message: "Error retrieving parent account from contact" });
     }
     
     return false;
@@ -133,6 +136,11 @@ function hideEmptyFields(formContext, contact) {
     
     if (contact.emailaddress1 == null)
         primaryContactForm.getControl("emailaddress1").setVisible(false);
+    else
+        primaryContactForm.getControl("emailaddress1").setVisible(true);
+    
     if (contact.mobilephone == null)
         primaryContactForm.getControl("mobilephone").setVisible(false);
+    else
+        primaryContactForm.getControl("mobilephone").setVisible(true);
 }
