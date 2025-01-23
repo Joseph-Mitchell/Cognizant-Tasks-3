6af7cc5593fa5213c37cad2dc38433f604c59bb6
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2025-01-23 10:28:12 +0000

    Added Scenario 2

---
+++ Scenario 2/CancelNewCaseIfExistingWithSameCustomer/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xrm.Sdk;
+
+namespace CancelNewCaseIfExistingWithSameCustomer
+{
+    public class Plugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            
+        }
+    }
+}
