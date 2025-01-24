e3430540f1bcc0053c48dce2b33c275848897225
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2025-01-24 11:32:16 +0000

    Scenario 1: Reordered functions

--- Scenario 1/showAccountPrimaryContact.js
+++ Scenario 1/showAccountPrimaryContact.js
@@ -16,6 +16,85 @@ async function showAccountPrimaryContact(executionContext) {
     }
 }
 
+async function retrieveContact(formContext) {
+    let customerAttribute = getCustomerAttribute(formContext);     
+    let customerRecord = getCustomerRecord(customerAttribute);
+    if (customerRecord.entityType != "account")
+        throw new Error();
+    
+    try {
+        let account = await Xrm.WebApi.retrieveRecord(
+            "account",
+            customerRecord.id,
+            "?$select=primarycontactid&$expand=primarycontactid($select=contactid,fullname,emailaddress1,mobilephone)"
+        );
+        return account.primarycontactid;
+    } catch {
+        throw new Error("There was an error retrieving account details.");
+    }
+}
+
+function getCustomerAttribute(formContext) {
+    let customerAttribute = formContext.getAttribute("customerid");
+    if (customerAttribute === null)
+        throw new Error("The Customer attribute could not be found in the Case table.");
+    
+    return customerAttribute;
+}
+
+function getCustomerRecord(customerAttribute) {
+    let customerRecordArray = customerAttribute.getValue();
+    if (customerRecordArray === null || customerRecordArray.length === 0)
+        throw new Error();
+    
+    return customerRecordArray[0];
+}
+
+function populateForm(formContext, contact) {   
+    let contactRecord = {
+        entityType: "contact",
+        id: contact.contactid,
+        name: contact.fullname
+    };
+    formContext.getAttribute("primarycontactid").setValue([contactRecord]);
+}
+
+function hideEmptyFields(formContext, contact) {
+    let primaryContactForm = formContext.ui.quickForms.get("accountcontact_qfc");
+    if (primaryContactForm === null)
+        throw new Error("The quick form 'accountcontact_qfc' could not be found");
+    
+    if (contact.emailaddress1 === null)
+        primaryContactForm.getControl("emailaddress1").setVisible(false);
+    else
+    primaryContactForm.getControl("emailaddress1").setVisible(true);
+
+if (contact.mobilephone === null)
+    primaryContactForm.getControl("mobilephone").setVisible(false);
+else
+primaryContactForm.getControl("mobilephone").setVisible(true);
+}
+
+//----- Shared functions -----
+
+function toggleContactMandatory(formContext, mandatory) {
+    let contactAttribute = formContext.getAttribute("primarycontactid");
+    let contactControl = contactAttribute.controls.get((control) => {
+        if (control.controlDescriptor.Label === "Account Primary Contact")
+            return true;
+    })[0];
+    
+    if (mandatory) {
+        contactAttribute.setRequiredLevel("required");
+        contactControl.setVisible(true);
+    } else {
+        contactAttribute.setRequiredLevel("none");
+        contactControl.setVisible(false);
+    }
+}
+
+//----- OnSave functions -----
+
 async function saveContactIfRequired(executionContext) {
     const formContext = executionContext.getFormContext();
 
@@ -67,79 +146,4 @@ async function saveAccountPrimaryContact(accountId, contactId) {
     } catch {
         Xrm.Navigation.openErrorDialog({ message: "Error saving primary contact to account" });
     }
-}
-
-async function retrieveContact(formContext) {
-    let customerAttribute = getCustomerAttribute(formContext);     
-    let customerRecord = getCustomerRecord(customerAttribute);
-    if (customerRecord.entityType != "account")
-        throw new Error();
-
-    try {
-        let account = await Xrm.WebApi.retrieveRecord(
-            "account",
-            customerRecord.id,
-            "?$select=primarycontactid&$expand=primarycontactid($select=contactid,fullname,emailaddress1,mobilephone)"
-        );
-        return account.primarycontactid;
-    } catch {
-        throw new Error("There was an error retrieving account details.");
-    }
-}
-
-function getCustomerAttribute(formContext) {
-    let customerAttribute = formContext.getAttribute("customerid");
-    if (customerAttribute === null)
-        throw new Error("The Customer attribute could not be found in the Case table.");
-    
-    return customerAttribute;
-}
-
-function getCustomerRecord(customerAttribute) {
-    let customerRecordArray = customerAttribute.getValue();
-    if (customerRecordArray === null || customerRecordArray.length === 0)
-        throw new Error();
-    
-    return customerRecordArray[0];
-}
-
-function toggleContactMandatory(formContext, mandatory) {
-    let contactAttribute = formContext.getAttribute("primarycontactid");
-    let contactControl = contactAttribute.controls.get((control) => {
-        if (control.controlDescriptor.Label === "Account Primary Contact")
-            return true;
-    })[0];
-    
-    if (mandatory) {
-        contactAttribute.setRequiredLevel("required");
-        contactControl.setVisible(true);
-    } else {
-        contactAttribute.setRequiredLevel("none");
-        contactControl.setVisible(false);
-    }
-}
-
-function populateForm(formContext, contact) {   
-    let contactRecord = {
-        entityType: "contact",
-        id: contact.contactid,
-        name: contact.fullname
-    };
-    formContext.getAttribute("primarycontactid").setValue([contactRecord]);
-}
-
-function hideEmptyFields(formContext, contact) {
-    let primaryContactForm = formContext.ui.quickForms.get("accountcontact_qfc");
-    if (primaryContactForm === null)
-        throw new Error("The quick form 'accountcontact_qfc' could not be found");
-    
-    if (contact.emailaddress1 === null)
-        primaryContactForm.getControl("emailaddress1").setVisible(false);
-    else
-        primaryContactForm.getControl("emailaddress1").setVisible(true);
-    
-    if (contact.mobilephone === null)
-        primaryContactForm.getControl("mobilephone").setVisible(false);
-    else
-        primaryContactForm.getControl("mobilephone").setVisible(true);
 }
